607580504aa22719366e938120da1ef53453b27e
Tomas Kral <tkral@redhat.com>
2017-12-01 14:29:04 +0100

    update for angular frontend

--- server.go
+++ server.go
@@ -27,9 +27,10 @@ var (
 const key string = "guestbook"
 
 type comment struct {
-	ID     bson.ObjectId `json:"id" bson:"_id,omitempty" `
-	Author string        `json:"author"`
-	Text   string        `json:"text"`
+	ID        bson.ObjectId `json:"id" bson:"_id,omitempty" `
+	Author    string        `json:"author"`
+	Text      string        `json:"text"`
+	Timestamp time.Time     `json:"timestamp"`
 }
 
 func handleComments(w http.ResponseWriter, r *http.Request) {
@@ -42,25 +43,26 @@ func handleComments(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Write([]byte{})
 	case "POST":
-		text := r.FormValue("text")
-		author := r.FormValue("author")
+		var data comment
+		err := json.NewDecoder(r.Body).Decode(&data)
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			log.Fatal(err)
+		}
 
-		newComment := comment{Author: author, Text: text}
+		log.Printf("data %#v\n", data)
 
 		c := database.C("comments")
 
-		err := c.Insert(&newComment)
+		data.ID = bson.NewObjectId()
+		data.Timestamp = time.Now()
+
+		err = c.Insert(&data)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		allComments := []comment{}
-
-		err = c.Find(nil).All(&allComments)
-		if err != nil {
-			log.Fatal(err)
-		}
-		output, err := json.MarshalIndent(allComments, "", "  ")
+		output, err := json.MarshalIndent(data, "", "  ")
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -76,10 +78,11 @@ func handleComments(w http.ResponseWriter, r *http.Request) {
 
 		allComments := []comment{}
 
-		err := c.Find(nil).All(&allComments)
+		err := c.Find(nil).Sort("-timestamp").All(&allComments)
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		output, err := json.MarshalIndent(allComments, "", "  ")
 		if err != nil {
 			log.Fatal(err)
@@ -133,6 +136,9 @@ func main() {
 		log.Printf("trying connect to db (attempt: %d/%d)", retry, maxRetry)
 		mongo, err = mgo.Dial(mongoServer)
 		log.Printf("Error: %#v\n", err)
+		if err == nil {
+			break
+		}
 
 		time.Sleep(5 * time.Second)
 		if retry >= maxRetry {
